979bb7c1f032d780be5c2647e7f457e43b030942
Antonio Horta Ribeiro <antonior92@gmail.com>
2021-05-04 19:32:10 +0200

    Implement parameter norm

--- random_feature_regression.py
+++ random_feature_regression.py
@@ -73,9 +73,9 @@ def compute_asymptotics(features_over_input_dim, samples_over_input_dim, activat
                       activation_params['E{fn(G)}']**2 - activation_params['E{G*fn(G)}']**2)
 
     zeta = activation_params['E{G*fn(G)}']**2 / mu_star**2
-    corrected_regularizaton = regularization / mu_star**2
+    corrected_regularizaton = regularization / mu_star **2
     xi_imag = np.sqrt(features_over_input_dim * samples_over_input_dim * corrected_regularizaton)
-
+    rho = snr**2
     psi1 = features_over_input_dim  # as used in Mei and Montanari - to make equations bellow easier to read!
     psi2 = samples_over_input_dim  # as used in Mei and Montanari - to make equations bellow easier to read!
 
@@ -84,12 +84,13 @@ def compute_asymptotics(features_over_input_dim, samples_over_input_dim, activat
     # Implements Eq (16) of Mei and Montanari
     # I am assuming here that chi is a real number. And that, except for numerical errors
     # imag(vs * vf) was supposed to be zero
-    chi = np.abs(vf * vs)
+    chi = -np.imag(vf) * np.imag(vs)
 
     def m(p, q):
         """implement chi zeta monomial in compact format."""
         return chi ** p * zeta ** q
 
+    # ---- Compute prediction risk ---- #
     # Implements eq (17) of Mei and Montanari
     E0 = - m(5, 6) + 3 * m(4, 4) + (psi1*psi2 - psi1 - psi2 + 1) * m(3, 6) - 2 * m(3, 4) - 3 * m(3, 2) + \
         (psi1 + psi2 - 3 * psi1 * psi2 + 1) * m(2, 4) + 2 * m(2, 2) + m(2, 0) + \
@@ -98,12 +99,20 @@ def compute_asymptotics(features_over_input_dim, samples_over_input_dim, activat
     E2 = m(5, 6) - 3 * m(4, 4) + (psi1 - 1) * m(3, 6) + 2 * m(3, 4) + 3 * m(3, 2) + \
          (-psi1 - 1) * m(2, 4) - 2 * m(2, 2) - m(2, 0)
 
-    B = E1 / E0  * 0  # Implements Eq (18) of Mei and Montanari
+    B = E1 / E0  # Implements Eq (18) of Mei and Montanari
     V = E2 / E0  # Implements Eq (19) of Mei and Montanari
-    R = snr**2 / (1 + snr**2) * B + 1 / (1 + snr**2) * V  # Implements Eq (20) of Mei and Montanari
+    R = rho / (1 + rho) * B + 1 / (1 + rho) * V  # Implements Eq (20) of Mei and Montanari
+    predicted_risk = noise_std ** 2 * ((snr**2 + 1) * R + 1)  # Implements LHS of Eq (5) of Mei and Montanari
 
-    predicted_risk = noise_std ** 2 * ((snr ** 2 + 1) * R + 1)  # Implements LHS of Eq (5) of Mei and Montanari
-    return predicted_risk
+    # ---- Compute parameter norm ---- #
+    # Implements Eq (48) of Mei and Montanari
+    A1 = - rho / (1 + rho) * m(2, 0) * ((1 - psi2) * m(1, 4) - m(1, 2) + (1 + psi2) * m(0, 2) + 1) + \
+         + 1 / (1 + rho) * m(2, 0) * (m(1, 2) - 1) * (m(2, 4) - 2 * m(1, 2) + m(0, 2) + 1)
+    A0 = E0
+    A = A1 / A0
+    parameter_norm = noise_std * np.sqrt((snr**2 + 1) * A) / mu_star
+
+    return predicted_risk, parameter_norm
 
 
 if __name__ == '__main__':
@@ -111,16 +120,16 @@ if __name__ == '__main__':
     from tqdm import tqdm
     import pandas as pd
 
-    input_dim = 10
-    n_samples = 200
+    input_dim = 400
+    n_samples = 300
     n_test_samples = 500
     seed = 2
-    snr = 1
+    snr = 4
     noise_std = 1
     activation_function = get_activation('relu')
     activation_params = activation_function_parameters('relu')
-    regularization = 1e-8
-    repetitions = 3
+    regularization = 1e-7
+    repetitions = 4
     lower_proportion = -0.99
     upper_proportion = 1
     num_points = 60
@@ -144,12 +153,14 @@ if __name__ == '__main__':
 
     proportions2 = np.logspace(lower_proportion, upper_proportion, 100)
     predicted_risk = []
+    parameter_norm = []
     for proportion in tqdm(proportions2):
         n_features = max(int(proportion * n_samples), 1)
-        r = compute_asymptotics(n_features/input_dim, n_samples/input_dim,
-                                activation_params, regularization, snr, noise_std,
-                                compute_vs)
+        r, n = compute_asymptotics(n_features/input_dim, n_samples/input_dim,
+                                   activation_params, regularization, snr, noise_std,
+                                   compute_vs)
         predicted_risk.append(r)
+        parameter_norm.append(n)
 
     # Plot
     import matplotlib.pyplot as plt
@@ -158,4 +169,13 @@ if __name__ == '__main__':
     ax.plot(proportions2, predicted_risk)
     ax.set_xscale('log')
     ax.set_yscale('log')
+    ax.set_title('prediction risk')
+    plt.show()
+
+    fig, ax = plt.subplots()
+    ax.plot(df['proportion'], df['l2_param_norm'], '*')
+    ax.plot(proportions2, np.array(parameter_norm))
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_title('parameter norm')
     plt.show()
