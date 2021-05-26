7f8be28e9c3f1b70543f0342f67a79d416247d28
Antonio Horta Ribeiro <antonior92@gmail.com>
2021-05-26 14:29:56 +0200

    Update generate figure script according to the new paper structure

--- linear_with_random_covariates/generate_figures.sh
+++ linear_with_random_covariates/generate_figures.sh
@@ -4,11 +4,10 @@ mkdir figures  # if it does not exist already
 STYLE="ggplot ../mystyle.mplsty"
 export PYTHONPATH="${PYTHONPATH}::../"
 
-echo "Generate data for Figures 1 and 2..."
-for ORD in 2 1.5 20 inf;
-do for TP in isotropic equicorrelated;
-  do
-    python adversarial_risk.py --num_test_samples 100 --num_train_samples 100 -o results/l"$ORD"-"$TP".csv --features_kind $TP --ord $ORD
-    python plot_double_descent.py --file results/l"$ORD"-"$TP".csv --plot_style $STYLE --y_max 1e5 --save figures/l"$ORD"-"$TP"
-  done;
+echo "Generate data for Figures 2..."
+TP=isotropic
+for ORD in 2 1.5 20;
+do
+  python adversarial_risk.py --num_test_samples 100 --num_train_samples 100 -o results/l"$ORD"-"$TP".csv --features_kind $TP --ord $ORD
+  python plot_double_descent.py --file results/l"$ORD"-"$TP".csv --plot_style $STYLE --y_max 1e5 --save figures/l"$ORD"-"$TP"
 done;
