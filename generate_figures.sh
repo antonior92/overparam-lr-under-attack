e271b2e00ad3aa91be97427a8ca50a0d05e2c664
antonio <antonior92@gmail.com>
2021-09-29 12:04:44 +0200

    Update example

--- generate_figures.sh
+++ generate_figures.sh
@@ -2,11 +2,10 @@ mkdir figures  # if it does not exist already
 STYLE="plot_style_files/mystyle.mplsty"
 
 echo "Generate Figures 1..."
-python plot_linear.py --file results/equicorrelated-constant.csv  --plot_style $STYLE plot_style_files/one_half.mplsty plot_style_files/mycolors.mplsty   --plot_type risk_per_eps --save figures/equicorrelated-constant.pdf
+python plot_linear.py --file results/equicorrelated-constant.csv  --plot_style $STYLE plot_style_files/one_half.mplsty plot_style_files/mycolors.mplsty   --plot_type risk_per_eps  --plot_type risk_per_eps --remove_bounds --second_marker_set --save figures/equicorrelated-constant.pdf
 
 echo "Generate Figures 2..."
 python plot_linear.py --file results/equicorrelated0.9-gaussian-prior.csv --save figures/equicorrelated-gaussian-prior-l2.pdf --plot_style $STYLE plot_style_files/one_half.mplsty  --ord 2 --y_min -0.2 --y_max 4.6
-
 
 echo "Generate Figures 3..."
 python plot_linear.py --file results/isotropic-gaussian-prior.csv --save figures/isotropic-gaussian-prior-l1.5.pdf --plot_style $STYLE plot_style_files/one_third_with_ylabel.mplsty --ord 1.5 --y_min -0.2 --y_max 4.6
