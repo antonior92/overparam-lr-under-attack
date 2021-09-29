4cd6526773759ec499f9a66f2f02748b71452c1f
antonio <antonior92@gmail.com>
2021-09-29 13:11:57 +0200

    Change names

--- linear-estimate.py
+++ linear-estimate.py
@@ -4,7 +4,85 @@ from tqdm import tqdm
 import pandas as pd
 import numpy as np  # numpy > 1.10 so we can use np.linalg.norm(...,axis=axis, keepdims=keepdims)
 import random
-from interp_under_attack.linear_with_random_covariates.adversarial_risk import train_and_evaluate
+from interp_under_attack. adversarial_attack import compute_adv_attack
+
+
+def generate_features(n_samples, n_features, rng, kind, off_diag):
+    # Get random components
+    z = rng.randn(n_samples, n_features)
+    if kind == 'isotropic':
+        return z
+    elif kind == 'equicorrelated':
+        s = (n_features, n_features)
+        cov = (1-off_diag) * np.eye(*s) + off_diag * np.ones(s)
+        # take square root
+        u, s, vh = np.linalg.svd(cov)
+        cov_sqr = np.dot(u * np.sqrt(s), vh)
+        # return
+        return z @ cov_sqr
+    else:
+        raise ValueError('Invalid kind of feature generation')
+
+
+def train_and_evaluate(n_samples, n_features, noise_std, parameter_norm, epsilon, ord,
+                       n_test_samples, kind, off_diag, datagen_parameter, seed):
+    # Get state
+    rng = np.random.RandomState(seed)
+
+    # Get parameter
+    if datagen_parameter == 'gaussian_prior':
+        beta = parameter_norm / np.sqrt(n_features) * rng.randn(n_features)
+    elif datagen_parameter == 'constant':
+        beta = parameter_norm / np.sqrt(n_features) * np.ones(n_features)
+
+    # Generate training data
+    # Get X matrix
+    X = generate_features(n_samples, n_features, rng, kind, off_diag)
+    # Get error
+    e = rng.randn(n_samples)
+    # Compute output
+    y = X @ beta + noise_std * e
+
+    # Train
+    beta_hat, _resid, _rank, _s = linalg.lstsq(X, y)
+
+    # Test data
+    # Get X matrix
+    X_test = generate_features(n_test_samples, n_features, rng, kind, off_diag)
+    # Get error
+    e_test = rng.randn(n_test_samples)
+    # Compute output
+    y_test = X_test @ beta + noise_std * e_test
+
+    # Generate adversarial disturbance
+    pnorms = {}
+    pnorms['norm-2.0'] = np.linalg.norm(beta_hat, ord=2)
+
+    # Compute error = y_pred - y_test
+    risk = {}
+    test_error = X_test @ beta_hat - y_test
+    risk['predrisk'] = np.mean(test_error ** 2)
+    for p in ord:
+        # Compute ord
+        if p != np.Inf and p > 1:
+            q = p / (p - 1)
+        elif p == 1:
+            q = np.Inf
+        else:
+            q = 1
+        pnorms['norm-{:.1f}'.format(p)] = np.linalg.norm(beta_hat, ord=q)
+
+        jac = beta_hat
+        delta_x = compute_adv_attack(test_error, jac, ord=p)
+        for e in epsilon:
+            # Estimate adversarial arisk
+            delta_X = e * delta_x
+            r = np.mean((y_test - (X_test + delta_X) @ beta_hat) ** 2)
+            risk['advrisk-{:.1f}-{:.1f}'.format(p, e)] = r
+
+    # Compute distance
+    l2distance = np.linalg.norm(beta_hat - beta, ord=2)
+    return risk, pnorms, l2distance
 
 
 if __name__ == '__main__':
--- linear-plot.py
+++ linear-plot.py
@@ -3,8 +3,91 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 
-from interp_under_attack.linear_with_random_covariates.asymptotics import asymptotic_risk, assymptotic_l2_norm_squared,\
-    lp_norm_squared, adversarial_bounds,l2_distance_between_parameters
+import numpy as np
+
+
+def asymptotic_risk(proportion, signal_amplitude, noise_std, features_kind, off_diag):
+    # This follows from Hastie Thm.1 (p.7) and is the same regardless of the covariance matrix
+
+    # The variance term
+    v_underparametrized = proportion / (1 - proportion)
+    v_overparametrized = 1 / (proportion - 1)
+    v = (proportion < 1) * v_underparametrized + (proportion > 1) * v_overparametrized
+
+    # The bias term
+    b_underparametrized = 0
+    if features_kind == 'isotropic':
+        b_overparametrized = (1 - 1 / proportion)
+    elif features_kind == 'equicorrelated':
+        b_overparametrized = (1 - off_diag) * (1 - 1 / proportion)
+    else:
+        raise ValueError
+    b = (proportion < 1) * b_underparametrized + (proportion > 1) * b_overparametrized
+
+    return noise_std ** 2 * v + signal_amplitude ** 2 * b
+
+
+def assymptotic_l2_norm_squared(proportion, signal_amplitude, noise_std, features_kind, off_diag):
+
+    if features_kind == 'isotropic':
+        v_underparametrized = proportion / (1 - proportion)
+        v_overparametrized = 1 / (proportion - 1)
+    elif features_kind == 'equicorrelated':
+        v_underparametrized = proportion / ((1 - proportion) * (1 - off_diag))
+        v_overparametrized = 1 / ((proportion - 1)* (1 - off_diag))
+    else:
+        raise ValueError
+    v = (proportion < 1) * v_underparametrized + (proportion > 1) * v_overparametrized
+
+    b_underparametrized = 1
+    b_overparametrized = 1 / proportion
+    b = (proportion < 1) * b_underparametrized + (proportion > 1) * b_overparametrized
+
+    return noise_std ** 2 * v + signal_amplitude ** 2 * b
+
+
+def l2_distance_between_parameters(proportion, signal_amplitude, noise_std, features_kind, off_diag):
+    if features_kind == 'isotropic':
+        v_underparametrized = proportion / (1 - proportion)
+        v_overparametrized = 1 / (proportion - 1)
+    elif features_kind == 'equicorrelated':
+        v_underparametrized = proportion / ((1 - proportion) * (1 - off_diag))
+        v_overparametrized = 1 / ((proportion - 1)* (1 - off_diag))
+    else:
+        raise ValueError
+    v = (proportion < 1) * v_underparametrized + (proportion > 1) * v_overparametrized
+
+    b_underparametrized = 0
+    b_overparametrized = 1 - 1 / proportion
+    b = (proportion < 1) * b_underparametrized + (proportion > 1) * b_overparametrized
+
+    return noise_std ** 2 * v + signal_amplitude ** 2 * b
+
+
+def lp_norm_squared(l2_norm_squared, ord, sz):
+    # Generalize to other norms,
+    # using https://math.stackexchange.com/questions/218046/relations-between-p-norms
+    if ord == np.inf:
+        factor = sz ** 1/2
+    else:
+        factor = sz ** (1/2-1/ord)
+
+    lfactor = 1 if ord >= 2 else factor
+    ufactor = 1 if ord <= 2 else factor
+
+    lower_bound = l2_norm_squared * lfactor ** 2
+    upper_bound = l2_norm_squared * ufactor ** 2
+
+    return lower_bound, upper_bound
+
+
+def adversarial_bounds(arisk, anorm, noise_std, eps, ord, n_features, datagen_parameter):
+    lqnorm_lb, lqnorm_ub = lp_norm_squared(anorm, ord, n_features)
+
+    upper_bound = (np.sqrt(arisk) + eps * np.sqrt(lqnorm_ub))**2 + noise_std ** 2
+    lower_bound = arisk + eps**2 * lqnorm_lb + noise_std ** 2
+
+    return lower_bound, upper_bound
 
 def plot_risk_and_bounds(i, lbl, p, e):
     r = df['advrisk-{:.1f}-{:.1f}'.format(p, e)]
