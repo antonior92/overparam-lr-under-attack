1de21ad499b7e85a73b5ee468627e00fc5e74900
antonio <antonior92@gmail.com>
2021-12-03 16:44:14 +0100

    organize script `experiment attacks`

--- experiment_attacks.sh
+++ experiment_attacks.sh
@@ -2,8 +2,9 @@
 mkdir out
 mkdir out/results  # if it does not exist already
 mkdir out/figures  # if it does not exist already
+mkdir out/figures/advtrain
 RESULTS=out/results
-FIGURES=out/figures
+FIGURES=out/figures/advtrain
 STYLE="plot_style_files/mystyle.mplsty"
 
 rep(){
@@ -18,34 +19,37 @@ python linear-estimate.py --num_test_samples $N --num_train_samples $N \
       --features_kind isotropic --signal_amplitude 4 \
       -o out/results/control-noreg
 
-N=100
 for REGUL_TYPE in advtrain-l2 advtrain-linf ridge;
 do
-  for REG in 0.5 0.05; #10 1  0.1 0.05 0.01 0.001;
+  for REG in 10 1 0.5 0.1 0.05 0.01 0.001;
   do python linear-estimate.py --num_test_samples $N --num_train_samples $N \
       --features_kind isotropic --signal_amplitude 4 --training $REGUL_TYPE --regularization $REG \
       -o out/results/"$REGUL_TYPE"-"$REG"
   done;
 done;
 
+for REGUL_TYPE in advtrain-l2 ridge;
+do
+  PLT_TYPE=advrisk
+  for EPS in 0 0.1 0.5 1.0 2.0;
+  do python linear-plot.py out/results/"$REGUL_TYPE"-{1,0.1,0.5,0.01,0.05} out/results/control-noreg --ord $(rep 2 6) \
+      --eps  $(rep $EPS 6)  --labels {1,0.5,0.1,0.05,0.01}  noreg --experiment_plot median_line\
+      --plot_type $PLT_TYPE --remove_bounds --save $FIGURES/"$PLT_TYPE"-"$REGUL_TYPE"-e"$EPS".pdf
+  done;
+  PLT_TYPE="train_mse"
+  python linear-plot.py out/results/"$REGUL_TYPE"-{1,0.1,0.5,0.01,0.05} out/results/control-noreg --ord $(rep 2 6) \
+      --eps  $(rep $EPS 6)  --labels {1,0.5,0.1,0.05,0.01}  noreg --experiment_plot median_line\
+      --plot_type $PLT_TYPE --remove_bounds --save $FIGURES/"$PLT_TYPE"-"$REGUL_TYPE".pdf
+done;
 
-python linear-plot.py out/results/control-noreg out/results/advtrain-l2-{1,0.1,0.5,0.01,0.05} --ord $(rep 2 6) \
-    --eps  $(rep 2.0 6)  --labels noreg {1,0.5,0.1,0.05,0.01} --experiment_plot error_bars \
-    --plot_type advrisk --remove_bounds
-python linear-plot.py out/results/control-noreg out/results/advtrain-l2-{1,0.1,0.5,0.01,0.05} --ord $(rep 2 6) \
-    --eps  $(rep 2.0 6)  --labels noreg {1,0.5,0.1,0.05,0.01} --experiment_plot error_bars \
-     --plot_type train_mse
-
-python linear-plot.py out/results/control-noreg out/results/ridge-{1,0.1,0.5,0.01,0.05} --ord $(rep 2 6) \
-    --eps  $(rep 2.0 6)  --labels noreg {1,0.5,0.1,0.05,0.01} --experiment_plot error_bars \
-    --plot_type advrisk --remove_bounds
-python linear-plot.py out/results/ridge-{1,0.1,0.5,0.01,0.05} --ord $(rep 2 5) \
-    --eps  $(rep 2.0 5)  --labels {1,0.5,0.1,0.05,0.01} --experiment_plot error_bars \
-     --plot_type train_mse
-
-python linear-plot.py out/results/control-noreg out/results/advtrain-linf-{10,1,0.1,0.01,0.001} --ord $(rep 2 6) \
-    --eps  $(rep 2.0 6)  --labels noreg {10,1,0.1,0.01,0.001} --experiment_plot error_bars \
-    --plot_type advrisk --remove_bounds
-python linear-plot.py out/results/control-noreg out/results/advtrain-linf-{10,1,0.1,0.01,0.001} --ord $(rep 2 6) \
-    --eps  $(rep 2.0 6)  --labels noreg {10,1,0.1,0.01,0.001} --experiment_plot error_bars \
-     --plot_type train_mse
+REGUL_TYPE=advtrain-linf
+PLT_TYPE=advrisk
+for EPS in 0 0.1 0.5 1.0 2.0;
+do python linear-plot.py out/results/"$REGUL_TYPE"-{10,1,0.1,0.01,0.001} out/results/control-noreg --ord $(rep 2 6) \
+    --eps  $(rep $EPS 6)  --labels {10,1,0.1,0.01,0.001} noreg --experiment_plot median_line\
+    --plot_type $PLT_TYPE --remove_bounds --save $FIGURES/"$PLT_TYPE"-"$REGUL_TYPE"-e"$EPS".pdf
+done;
+PLT_TYPE="train_mse"
+python linear-plot.py out/results/"$REGUL_TYPE"-{10,1,0.1,0.01,0.001} out/results/control-noreg --ord $(rep 2 6) \
+    --eps  $(rep $EPS 6)  --labels {10,1,0.1,0.01,0.001}  noreg --experiment_plot median_line\
+    --plot_type $PLT_TYPE --remove_bounds --save $FIGURES/"$PLT_TYPE"-"$REGUL_TYPE".pdf
